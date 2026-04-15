b129a3c32e1e3905c7366c9750955399200374f0
mathiasabarca-ux <mathiasabarca@liceovvh.cl>
2026-04-15 08:10:37 -0400

    ok

---
+++ 01_introduccion/hola_mundo/static/js/script.js
@@ -0,0 +1,11 @@
+console.log("Hola mundo, desde archivo externo!")
+
+
+// 1) Hola mundo con variable y const.
+const saludo =  "hola a todos xd"
+let destinatario = "estudiantes"
+
+console.log(saludo + " para todos " + destinatario);
+
+// 2) Saludar a una persona ingresada por el usuario.
+let nombre = prompt("¿Cuál es tu nombre?");
---
+++ 02_Tipos_de_datos/1.-tipo_texto/static/js/script.js
@@ -0,0 +1,10 @@
+console.log("conexion con js correcta");
+/*
+Dato: Tipo de texto (string)
+concatenacion de texto con el signo +
+podemos unir textos y variables
+*/
+const nombre = "matghias"
+const apellido = "abarca"
+//unimos ambas constantes con un texto extra.
+console.log("hola " + nombre + " " + apellido);
